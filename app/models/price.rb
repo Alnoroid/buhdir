9583230a491050a1e19422cd8bb2cb7ed673e34d
Alnoroid <alnoroid@gmail.com>
2015-08-28 02:33:05 +0300

    Сделал возможность добавлять полноценно прайс. Ура. С картинками даже)

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,5 +1,5 @@
 class Price < ActiveRecord::Base
   belongs_to :event_price
 scope :titlesearch, -> (search) { where('title LIKE ? OR id LIKE ?', "%#{search}%","#{search}")}
-
+  scope :catsearch, -> (cat_id) {where(price_category_id: cat_id)}
 end
