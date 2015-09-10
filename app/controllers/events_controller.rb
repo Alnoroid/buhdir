433d04f1bf75099c6eb809e802f0b29ccf937e33
Alnoroid <alnoroid@gmail.com>
2015-09-10 09:19:53 +0300

    Сделал чтобы уведомления были посередине

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -80,7 +80,7 @@ class EventsController < ApplicationController
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to action: "index", notice: 'Мероприятие успешно создано.' }
+        format.html { redirect_to events_url, notice: 'Мероприятие успешно создано.' }
         format.json { render :show, status: :created, location: @event }
       else
         format.html { render :new }
