6996486ca88a4a4eb6d9cde56ff5ba3297cd2e26
Alnoroid <alnoroid@gmail.com>
2015-10-07 10:46:13 +0300

    Перевёл картотеку,надпись добавить наверх, и сделал чтоб не цифрами а словами было написано что это и откуда

--- app/models/contact_type.rb
+++ app/models/contact_type.rb
@@ -1,3 +1,4 @@
 class ContactType < ActiveRecord::Base
   belongs_to :contact_category
+  has_many :contacts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   belongs_to :role
   has_many :event_curator_users
+  has_many :contacts
 
   has_attached_file :userpic, :styles => { :medium => "300x300>", :thumb => "75x75>" }, :default_url => ":rails_root/app/assets/images/users/userpic.png", :path => ":rails_root/app/assets/images/users/:id/:style/:filename", :url => ":rails_root/app/assets/images/users/:id/:style/:filename"
   validates_attachment_content_type :userpic, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
